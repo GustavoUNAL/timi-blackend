396f2e6d997c4dc648978374a46300018f6477c5
GustavoUNAL <garteagae@unal.edu.co>
2025-01-02 08:01:02 -0500

    fix cors

--- src/main.py
+++ src/main.py
@@ -13,17 +13,26 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title=APP_NAME, debug=DEBUG_MODE)
 
 # Configurar CORS (ajusta según tus necesidades)
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    # Agrega dominios o puertos adicionales si tu front corre en otro lugar
-]
+# origins = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     # Agrega dominios o puertos adicionales si tu front corre en otro lugar
+# ]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://3.128.32.128:3000", "https://timi-time.online"],  # Orígenes permitidos
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
 )
 
 # Incluir los routers de categorías y tareas
